9980500dd6a46b2a9c6657962a673ee4757bc17b
francesfu <francesfu1024@outlook.com>
2025-12-27 16:31:44 +0000

    make http request in angular

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,15 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = 'Dating app';
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completed the http request'),
+    });
+  }
 }
