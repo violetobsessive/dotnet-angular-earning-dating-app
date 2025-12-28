38e50b7c44cb209584d9c37de623cc02504f051a
francesfu <francesfu1024@outlook.com>
2025-12-28 18:01:14 +0000

    add base controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,15 +1,13 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    public class MembersController(AppDbContext context) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
