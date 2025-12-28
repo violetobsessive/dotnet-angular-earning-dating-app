e3b1a6c69e7594abcc944f031711a5dad39995ff
francesfu <francesfu1024@outlook.com>
2025-12-28 17:53:44 +0000

    set password security in db

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace API.Entities;
 
 public class AppUser
@@ -5,4 +7,12 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    
+    // Add security to user passwords
+
+    // Encrpt password to hash
+    public required byte[] PasswordHash { get; set; }
+    
+    // Add random generated salt to hash
+    public required byte[] PasswordSalt { get; set; }
 }
