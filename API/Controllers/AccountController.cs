0f403982a75977f1e72f255f2f4f9b6683fc104d
francesfu <francesfu1024@outlook.com>
2025-12-29 23:02:49 +0000

    add hash and salt to password to a new endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+    [HttpPost("register")] //api/account/register
+    public async Task<ActionResult<AppUser>>Register(string email, string displayName, 
+    string password)
+    {
+        // hash password
+        using var hmac = new HMACSHA512();
+        
+        var user = new AppUser
+        {
+            DisplayName = displayName,
+            Email = email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key
+        };
+
+        // Add user to DB
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+}
