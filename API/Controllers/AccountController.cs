7d6b58b87aa220ddb2f0446d76ecc21e7adfaf57
francesfu <francesfu1024@outlook.com>
2026-01-18 22:35:39 +0000

    create RegisterDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,25 +2,27 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")] //api/account/register
-    public async Task<ActionResult<AppUser>>Register(string email, string displayName, 
-    string password)
+    public async Task<ActionResult<AppUser>>Register(RegisterDto registerDto)
     {
+        if(await EmailExists(registerDto.Email)) return BadRequest("Email already exists!");
         // hash password
         using var hmac = new HMACSHA512();
         
         var user = new AppUser
         {
-            DisplayName = displayName,
-            Email = email,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -29,5 +31,10 @@ public class AccountController(AppDbContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+    // Helper method to see if an email address already exists
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
 }
