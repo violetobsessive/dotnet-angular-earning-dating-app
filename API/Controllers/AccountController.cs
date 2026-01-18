55864b412f04603e3113bb5e9bba7f41e637d0b7
francesfu <francesfu1024@outlook.com>
2026-01-18 23:08:06 +0000

    add login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,4 +37,22 @@ public class AccountController(AppDbContext context) : BaseApiController
     {
         return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+        
+        if(user == null) return Unauthorized("Invalid email address!");
+        
+        // Compare the hashed version from DB with hashed version passed in from password in loginDto
+        // Supply password salt, so it creates same key to create hashed password
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var compustedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        for(var i = 0; i < compustedHash.Length; i++)
+        {
+            if(compustedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password!");
+        }
+        return user;
+    }
 }
