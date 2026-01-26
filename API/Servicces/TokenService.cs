c474b1425ea470379e3d9062fdc6f27516a00e7b
francesfu <francesfu1024@outlook.com>
2026-01-26 23:35:01 +0000

    add issuer and audience in config and token

--- API/Servicces/TokenService.cs
+++ API/Servicces/TokenService.cs
@@ -21,13 +21,27 @@ public class TokenService(IConfiguration config):ITokenService
             new Claim(ClaimTypes.Email, user.Email),
             new Claim(ClaimTypes.NameIdentifier, user.Id)
         };
+        // holds key from SymmetricSecurityKey and algorithm
+        // JWT third part signature is computed using these credentials
         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        
+        var issuer = config["Jwt:Issuer"];
+        var audience = config["Jwt:Audience"];
+        
+        //a blueprint/config object describing what to put into the token
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            // Identity/claims that become the token payload
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddDays(7),
+            
+            Issuer = issuer,               
+            Audience = audience,
+            
+            // How it should be signed
             SigningCredentials = credentials
         };
+        // Component that creates and writes/reads JWTs
         var tokenHandler = new JwtSecurityTokenHandler();
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
