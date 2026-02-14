d62293ebb6b3e59808f3a219a5a7ad58828ef3e1
francesfu <francesfu1024@outlook.com>
2026-02-14 23:43:46 +0000

    add account service

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+
+  login(creds: any) {
+    return this.http.post(this.baseUrl + 'account/login', creds);
+  }
 }
